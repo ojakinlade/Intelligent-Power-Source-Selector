ac6a74dd6a1eb930a9c31671b7b8d6012a7dc73c
JosephAkinlade <josebanky11@gmail.com>
2023-04-01 09:06:18 +0100

    Update utility.ino

--- Utility/utility/utility.ino
+++ Utility/utility/utility.ino
@@ -1,3 +1,6 @@
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
 #include <LiquidCrystal_I2C.h>
 #include "hc12.h"
 
@@ -14,10 +17,48 @@ typedef  struct
   uint16_t node2Pwr;
 }pwr_t;
 
+typedef struct
+{
+  uint32_t hour;
+  uint32_t minute;
+}tim_t;
+
 //RTOS Handle(s)
 TaskHandle_t appTaskHandle;
 TaskHandle_t nodeTaskHandle;
+TaskHandle_t webServerTaskHandle;
 QueueHandle_t nodeToAppQueue;
+
+AsyncWebServer server(80);
+
+const char* ssid = "AkJoe";
+const char* pword = "spacetek";
+
+const char* PARAM_INPUT_1 = "HOUR";
+const char* PARAM_INPUT_2 = "MINUTE";
+
+tim_t Time;
+
+// HTML web page to handle  input fields (Hour, minute)
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html><head>
+  <title>ESP Input Form</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  </head><body>
+  <form action="/get">
+    Hour: <input type="text" name="HOUR">
+    <input type="submit" value="Submit">
+  </form><br>
+  
+  <form action="/get">
+    Minute: <input type="text" name="MINUTE">
+    <input type="submit" value="Submit">
+  </form>
+</body></html>)rawliteral";
+
+void notFound(AsyncWebServerRequest *request) {
+  request->send(404, "text/plain", "Not found");
+}
 
 void setup() {
   // put your setup code here, to run once:
@@ -35,12 +76,75 @@ void setup() {
   }
   //Tasks
   xTaskCreatePinnedToCore(NodeTask,"",30000,NULL,1,&nodeTaskHandle,1);
-  xTaskCreatePinnedToCore(ApplicationTask,"",30000,NULL,1,&appTaskHandle,1);  
+  xTaskCreatePinnedToCore(ApplicationTask,"",25000,NULL,1,&appTaskHandle,1);  
+  xTaskCreatePinnedToCore(WebServerTask,"",3000,NULL,1,&webServerTaskHandle,1);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+}
+
+void WebServerTask(void* pvParameter)
+{
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid,pword);
+  if(WiFi.waitForConnectResult() != WL_CONNECTED)
+  {
+    Serial.println("WiFi connect Failed");
+    return;
+  }
+  Serial.println();
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+  //Send webpage with required fields to client
+  server.on("/",HTTP_GET,[](AsyncWebServerRequest* request)
+  {
+    request->send_P(200,"text/html",index_html);
+  });
+
+  // Send a GET request to <ESP_IP>/get?input1=<inputMessage>
+  server.on("/get", HTTP_GET, [] (AsyncWebServerRequest *request)
+  {
+    String inputMessage;
+    String inputParam;
+    // GET input1 value on <ESP_IP>/get?input1=<inputMessage>
+    if (request->hasParam(PARAM_INPUT_1)) 
+    {
+      inputMessage = "";
+      inputMessage = request->getParam(PARAM_INPUT_1)->value();
+      Time.hour = inputMessage.toInt();
+      inputParam = PARAM_INPUT_1;
+    }
+    // GET input2 value on <ESP_IP>/get?input2=<inputMessage>
+    else if (request->hasParam(PARAM_INPUT_2))
+    {
+      inputMessage = "";
+      inputMessage = request->getParam(PARAM_INPUT_2)->value();
+      Time.minute = inputMessage.toInt();
+      inputParam = PARAM_INPUT_2;
+    }
+    else 
+    {
+      inputMessage = "No message sent";
+      inputParam = "none";
+    }
+    Serial.println(inputMessage);
+    Serial.print("Hour: ");
+    Serial.println(Time.hour);
+    Serial.print("Minute: ");
+    Serial.println(Time.minute);
+    request->send(200, "text/html", "HTTP GET request sent to your ESP on input field (" 
+                                     + inputParam + ") with value: " + inputMessage +
+                                     "<br><a href=\"/\">Return to Home Page</a>");
+  });
+  server.onNotFound(notFound);
+  server.begin();
+
+  while(1)
+  {
+    vTaskDelay(pdMS_TO_TICKS(1000));
+  }
 }
 
 void ApplicationTask(void* pvParameters)
@@ -50,13 +154,25 @@ void ApplicationTask(void* pvParameters)
 
   lcd.init();
   lcd.backlight();
-  lcd.print(" ENERGY MONITOR");
+  lcd.setCursor(4,0);
+  lcd.print("INTELLIGENT");
+  lcd.setCursor(4,1);
+  lcd.print("POWER SOURCE");
+  lcd.setCursor(6,2);
+  lcd.print("SELECTOR");
+  lcd.setCursor(7,3);
+  lcd.print("(IPSS)");
   vTaskDelay(pdMS_TO_TICKS(1500));
   lcd.clear();
   lcd.print("INITIALIZING...");
   vTaskDelay(pdMS_TO_TICKS(1500));
   lcd.clear();
   vTaskResume(nodeTaskHandle);
+
+  const uint8_t displayState1 = 0;
+  const uint8_t displayState2 = 1;
+  uint8_t displayState = displayState1;
+  uint32_t prevTime = millis();
 
   while(1)
   {
@@ -66,21 +182,46 @@ void ApplicationTask(void* pvParameters)
       Serial.println("--Application task received data from Node task\n");
     }
     float totalPwr = pwr.node1Pwr + pwr.node2Pwr;
-    lcd.setCursor(0,0);
-    lcd.print("Node 1: ");
-    lcd.print(pwr.node1Pwr / 100.0,2);
-    lcd.print(" W");
-    lcd.setCursor(0,1);
-    lcd.print("Node 2: ");
-    lcd.print(pwr.node2Pwr / 100.0,2);
-    lcd.print(" W");
-    lcd.setCursor(0,2);
-    lcd.print("Total PWR: ");
-    lcd.print(totalPwr / 100.0);
-    lcd.print(" W");
-    lcd.setCursor(0,3);
-    lcd.print("Source used: ");
-    lcd.print("xxxx");
+    switch(displayState)
+    {
+      case displayState1:
+        lcd.setCursor(0,0);
+        lcd.print("Time: ");
+        lcd.print(Time.hour);
+        lcd.print(":");
+        lcd.print(Time.minute);
+        lcd.setCursor(0,1);
+        lcd.print("Node 1: ");
+        lcd.print(pwr.node1Pwr / 100.0,2);
+        lcd.print(" W");
+        lcd.setCursor(0,2);
+        lcd.print("Node 2: ");
+        lcd.print(pwr.node2Pwr / 100.0,2);
+        lcd.print(" W");
+        lcd.setCursor(0,3);
+        lcd.print("Total PWR: ");
+        lcd.print(totalPwr / 100.0);
+        lcd.print(" W");
+        if(millis() - prevTime >= 4000)
+        {
+          displayState = displayState2;
+          prevTime = millis();
+          lcd.clear(); 
+        }
+        break;
+
+      case displayState2:
+        lcd.setCursor(0,0);
+        lcd.print("Source used: ");
+        lcd.print("xxxx");
+        if(millis() - prevTime >= 4000)
+        {
+          displayState = displayState1;
+          prevTime = millis();
+          lcd.clear(); 
+        }
+        break;
+    }
   }
 }
 
